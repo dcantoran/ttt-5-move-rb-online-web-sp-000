95a0108fa0fd1ae0011021922ef822c501aa0505
David Cantoran <davidcantoran36@gmail.com>
2019-09-15 23:38:10 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(idx)
+  idx = idx.to_i - 1 
+end 
+
+def move(board_arr, idx, players_char = "X")
+  board_arr[idx] = players_char
+end 
